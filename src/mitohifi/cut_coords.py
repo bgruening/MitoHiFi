5b4fa5e41a154192234e8d81d8189f3ee5547cf2
Bjoern Gruening <bjoern.gruening@gmail.com>
2021-01-24 21:55:42 +0100

    use black

--- src/mitohifi/cut_coords.py
+++ src/mitohifi/cut_coords.py
@@ -5,7 +5,7 @@ from Bio import SeqIO
 import sys
 
 input_fasta = sys.argv[1]
-circu_info = open('circularisationCheck.txt', 'r')
+circu_info = open("circularisationCheck.txt", "r")
 
 file = circu_info.read()
 obj = file.split(",")
@@ -13,9 +13,9 @@ obj = file.split(",")
 if obj[1] == " True":
     record = SeqIO.read(input_fasta, "fasta")
     id = record.id
-    get = record[int(obj[3]):]
-    print(get.format('fasta'))
+    get = record[int(obj[3]) :]
+    print(get.format("fasta"))
 else:
     record = SeqIO.read(input_fasta, "fasta")
-    id = record.id  
-    print(record.format('fasta'))
+    id = record.id
+    print(record.format("fasta"))
